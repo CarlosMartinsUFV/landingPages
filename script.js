8503540a95988216f2e41ace353c53e5772f8528
CarlosMartins <carlosmartinsufv15@gmail.com>
2024-01-28 19:38:34 -0300

    LandingPage

---
+++ script.js
@@ -0,0 +1,13 @@
+// let body = document.querySelector("body");
+// let circulo = document.querySelector(".teste");
+
+// function verificarPosicao(e) {
+//   circulo.style.top = e.pageY + "px";
+
+//   circulo.style.left = e.pageX + "px";
+
+//   console.log(e);
+//   // circulo.innerText = e.target.innerText;
+// }
+
+// body.addEventListener("mousemove", verificarPosicao);
